611766473aca25d4862698e7928689bcf5d77c8f
Ankit Kumar Soni <blackstocks@Ankits-MacBook-Air.local>
2024-12-05 19:38:09 +0530

    page.tsx bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-"use client";
+// src/app/view/[accessKey]/page.tsx
 import DocumentViewer from "@/components/DocumentViewer";
 
-export default function ViewPage({
-  params,
-}: {
-  params: { accessKey: string };
-}) {
+interface PageProps {
+  params: {
+    accessKey: string;
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+export default function ViewPage({ params }: PageProps) {
   return (
     <main className="min-h-screen bg-gray-50">
       <DocumentViewer accessKey={params.accessKey} />
