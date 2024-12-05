b363c2ccd242d505b4be51d0b6cb5a3946c32b05
Ankit Kumar Soni <blackstocks@Ankits-MacBook-Air.local>
2024-12-05 19:59:50 +0530

    accesskey bug fixed

--- src/app/view/[accessKey]/page.tsx
+++ src/app/view/[accessKey]/page.tsx
@@ -1,7 +1,10 @@
 "use client";
+import { PageProps } from 'next/types';
 import DocumentViewer from '@/components/DocumentViewer';
 
-export default function ViewPage({ params }: { params: { accessKey: string } }) {
+export default async function ViewPage({
+  params,
+}: PageProps<{ accessKey: string }>) {
   return (
     <main className="min-h-screen bg-gray-50">
       <DocumentViewer accessKey={params.accessKey} />
